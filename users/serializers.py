0b3e6d9144c8f69887f847d212fd034d0da688c2
Mehdy Khoshnoody <mehdy.khoshnoody@gmail.com>
2020-02-21 01:25:56 +0330

    Add user serializer

---
+++ users/serializers.py
@@ -0,0 +1,46 @@
+from django.contrib.auth import password_validation
+from django.core.exceptions import ValidationError
+from rest_framework import serializers
+
+from .models import User
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password",
+            "last_login",
+            "created_at",
+            "updated_at",
+        )
+        read_only_fields = ("id", "last_login", "created_at", "updated_at")
+        extra_kwargs = {"password": {"write_only": True}}
+        depth = 1
+
+    def validate_password(self, value):
+        try:
+            password_validation.validate_password(value)
+        except ValidationError as e:
+            raise serializers.ValidationError(e.messages)
+        return value
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = super(UserSerializer, self).create(validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        """
+        Update User with validated data
+        """
+        if "password" in validated_data:
+            instance.set_password(validated_data.pop("password"))
+        return super(UserSerializer, self).update(instance, validated_data)
