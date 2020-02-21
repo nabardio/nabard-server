ad437ecba2b318bcc9cbcc56498ca9fd88b17219
Mehdy <mehdy.khoshnoody@gmail.com>
2020-02-21 19:31:51 +0330

    Add game admin panel configuration

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Game
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    readonly_fields = ("id", "created_at", "updated_at")
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    readonly_fields,
+                    "owner",
+                    "name",
+                    "description",
+                    "instruction",
+                    "code",
+                )
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("owner", "name", "description", "instruction", "code"),
+            },
+        ),
+    )
+
+    list_display = ("name", "description", "owner")
+    ordering = ("name",)
+    search_fields = ("name", "description")
+
