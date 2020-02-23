21b8cf30913b3f81a212b0e14e2c3e0538db0c3e
Mehdy Khoshnoody <mehdy.khoshnoody@gmail.com>
2020-02-24 00:58:03 +0330

    Add tests for robot APIs

--- conftest.py
+++ conftest.py
@@ -4,6 +4,14 @@ import pytest
 from django.core.files import File
 
 from games.models import Game
+from robots.models import Robot
+
+
+def generate_sample_file():
+    f = BytesIO("print('hello, world!')".encode())
+    f.name = "test.py"
+    f.seek(0)
+    return f
 
 
 @pytest.fixture(autouse=True)
@@ -33,24 +41,42 @@ def logged_in_client(sample_user, client):
 
 @pytest.fixture
 def sample_code():
-    file = BytesIO("print('hello, world!')".encode())
-    file.name = "test.py"
-    file.seek(0)
-    return file
+    return generate_sample_file()
 
 
 @pytest.fixture
-def sample_game(django_user_model, sample_user, sample_code):
+def sample_game(django_user_model, sample_user):
+    code = generate_sample_file()
+
     owner = django_user_model.objects.get(username=sample_user["username"])
     game_input = {
         "name": "test_game",
         "description": "description for test_game",
         "instruction": "long text describing how the game works and what to do?",
-        "code": File(sample_code),
+        "code": File(code),
         "owner": owner,
     }
 
     game = Game.objects.create(**game_input)
-    game_input["id"] = game.pk
+    game_input["id"] = str(game.pk)
 
     return game_input
+
+
+@pytest.fixture
+def sample_robot(django_user_model, sample_user, sample_game):
+    code = generate_sample_file()
+
+    owner = django_user_model.objects.get(username=sample_user["username"])
+    game = Game.objects.get(pk=sample_game["id"])
+    robot_input = {
+        "name": "test_robot",
+        "game": game,
+        "code": File(code),
+        "owner": owner,
+    }
+
+    robot = Robot.objects.create(**robot_input)
+    robot_input["id"] = str(robot.pk)
+
+    return robot_input
--- robots/tests.py
+++ robots/tests.py
@@ -1,3 +1,88 @@
-from django.test import TestCase
+from django.test.client import MULTIPART_CONTENT, BOUNDARY, encode_multipart
 
-# Create your tests here.
+
+def test__register_new_user(logged_in_client, sample_game):
+    robot_input = {"name": "test-robot", "game": sample_game["id"]}
+
+    resp = logged_in_client.post(
+        "/api/v1/robot/", data=robot_input, content_type="application/json"
+    )
+    robot_result = resp.json()
+
+    assert resp.status_code == 201
+    assert robot_result["name"] == robot_input["name"]
+
+
+def test__list_my_robots(logged_in_client, sample_robot):
+    resp = logged_in_client.get(
+        f"/api/v1/robot/?owner={str(sample_robot['owner'].pk)}",
+        content_type="application/json",
+    )
+
+    assert resp.status_code == 200
+
+    result = resp.json()
+    for item in result["results"]:
+        assert item["name"] == sample_robot["name"]
+
+
+def test__list_robots(client, sample_robot):
+    resp = client.get("/api/v1/robot/", content_type="application/json")
+
+    assert resp.status_code == 200
+
+    result = resp.json()
+    for item in result["results"]:
+        assert item["name"] == sample_robot["name"]
+
+
+def test__retrieve_robot(client, sample_robot):
+    resp = client.get(
+        f"/api/v1/robot/{sample_robot['id']}/", content_type="application/json"
+    )
+
+    result = resp.json()
+    assert resp.status_code == 200
+    assert result["name"] == sample_robot["name"]
+
+
+def test__upload_code_for_robot(logged_in_client, sample_code, sample_robot):
+    resp = logged_in_client.put(
+        f"/api/v1/robot/{sample_robot['id']}/code/",
+        encode_multipart(BOUNDARY, {"file": sample_code}),
+        content_type=MULTIPART_CONTENT,
+        format="multipart",
+    )
+    assert resp.status_code == 204
+
+
+def test__update_robot(logged_in_client, sample_robot):
+    resp = logged_in_client.put(
+        f"/api/v1/robot/{sample_robot['id']}/",
+        data={"name": "changed", "game": str(sample_robot["game"].pk)},
+        content_type="application/json",
+    )
+    robot_result = resp.json()
+
+    assert resp.status_code == 200
+    assert robot_result["name"] == "changed"
+
+
+def test__partial_update_user(logged_in_client, sample_robot):
+    resp = logged_in_client.patch(
+        f"/api/v1/robot/{sample_robot['id']}/",
+        data={"name": "changed"},
+        content_type="application/json",
+    )
+    robot_result = resp.json()
+
+    assert resp.status_code == 200
+    assert robot_result["name"] == "changed"
+
+
+def test__delete_robot(logged_in_client, sample_robot):
+    resp = logged_in_client.delete(
+        f"/api/v1/robot/{sample_robot['id']}/", content_type="application/json",
+    )
+
+    assert resp.status_code == 204
