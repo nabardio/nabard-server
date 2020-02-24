6d5a9fcea5cd2e28367f096701f6199e58950842
Mehdy Khoshnoody <mehdy.khoshnoody@gmail.com>
2020-02-24 23:46:15 +0330

    Add match admin panel configuration

--- matches/admin.py
+++ matches/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
 
-# Register your models here.
+from nabard.admin import Admin
+
+from . import models
+
+
+@admin.register(models.Match)
+class MatchAdmin(Admin):
+    readonly_fields = (
+        "id",
+        "created_at",
+        "updated_at",
+    )
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    readonly_fields,
+                    "start_at",
+                    "finished_at",
+                    "game",
+                    "home_robot",
+                    "away_robot",
+                    "home_score",
+                    "away_score",
+                    "runner_log",
+                )
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("start_at", "game", "home_robot", "away_robot"),
+            },
+        ),
+    )
+
+    list_display = ("home_robot", "away_robot", "game", "start_at", "finished_at")
+    list_filter = ("home_robot", "away_robot", "game", "start_at", "finished_at")
+    ordering = ("finished_at",)
+
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            return self.add_fieldsets
+        return super().get_fieldsets(request, obj)
